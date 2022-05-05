b39f3ac029c49a4b8417fb98e2c5b867fc23bb70
meltsplit <seokwoo2000@naver.com>
2022-05-06 03:40:37 +0900

    🗂 프로젝트 생성 및 폴더링

---
+++ Clone_CarrotMarket/Source/Support/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Clone_CarrotMarket
+//
+//  Created by 장석우 on 2022/05/06.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        
+        return true
+    }
+
+
+
+}
+
